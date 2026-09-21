c6c3b6892c7a4506ef5ce138d74bf205e2a6c231
thecoaty <xkzin_@hotmail.com>
2026-09-21 16:33:59 -0300

    finalizando intro a colecoes de dados

---
+++ IntroAColecoes/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+
+var diasDaSemana = new DiaDaSemana(); 
+
+var carrinho = new List<Produto>{
+new Produto() {Nome = "Leite", Preco = 7.89},
+new Produto() {Nome = "Manteiga", Preco = 3.45}
+};
+
+void PercorrendoComForEach(){
+    foreach(var produto in carrinho){
+        Console.WriteLine($"Produto: {produto.Nome}");
+    }
+}
+
+void PercorrendoDiasDaSemana(){
+    foreach(var dia in diasDaSemana){
+        Console.WriteLine(dia);
+    }
+}
+class Produto{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
+
+class DiaDaSemana : IEnumerable<string>
+{
+    public IEnumerator<string> GetEnumerator()
+    {
+        yield return "Domingo";
+        yield return "Segunda";
+        yield return "Terça";
+        yield return "Quarta";
+        yield return "Quinta";
+        yield return "Sexta";
+        yield return "Sábado";
+
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
